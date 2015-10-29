fef363e361b513cb2f5492f2dca9d3c3eb352590
Habib <aftzaman@gmail.com>
2015-10-29 16:42:35 +0530

    db url updated

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 if(!GLOBAL.portal.mongoose)
 {
     GLOBAL.portal.mongoose = require('mongoose');
-    GLOBAL.portal.mongoose.connect('mongodb://192.168.1.13/myOffer');
+    GLOBAL.portal.mongoose.connect(process.env.MONGOLAB_URI||'mongodb://192.168.1.13/myOffer');
 }
 
 module.exports.User = GLOBAL.portal.mongoose.model("User", require("./user.js").userSchema, "user");
